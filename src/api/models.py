719baca0c0d1f2043b1063f6ce461e7121191947
andlicon <andresjzabalac@hotmail.com>
2023-06-20 08:54:39 -0400

    ADD: data base created

--- src/api/models.py
+++ src/api/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    salt = db.Column(db.String(80), nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -14,6 +14,5 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "email": self.email
         }
