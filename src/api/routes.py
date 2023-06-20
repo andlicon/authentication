560e5cb0a18fb9331852434b1a3f7b2bf486ac8d
andlicon <andresjzabalac@hotmail.com>
2023-06-20 10:36:30 -0400

    ADD: login successful ADD: login generate a token

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException, is_valid_email, is_valid_password, set_password, check_password, generate_salt
+from flask_jwt_extended import create_access_token
 
 api = Blueprint('api', __name__)
 
-@api.route('/login', methods=['POST'])
+
+@api.route('/singup', methods=['POST'])
 def add_user():
     if not request.is_json:
         return jsonify({'message': 'Body must be a valid json item'}), 400
@@ -32,3 +34,26 @@ def add_user():
     except Exception as exception:
         db.session.rollback()
         return jsonify({'message': f'Error {exception.args}'}), 500
+
+
+@api.route('/login', methods=['POST'])
+def login():
+    if not request.is_json:
+        return jsonify({'message': 'Body must be a valid json item'}), 400
+
+    body = request.json
+    email = body.get('email', None)
+    password = body.get('password', None)
+
+    user = User.query.filter_by(email=email).one_or_none()
+    if user is None:
+        return jsonify({'message': 'Wrong password or email'}), 400
+
+    user_password = user.password
+    user_salt = user.salt
+    if not check_password(user_password, f'{password}{user_salt}'):
+        return jsonify({'message': 'Wrong password or email'}), 400
+
+    token = create_access_token(identity=user.id)
+
+    return jsonify({'token': token}), 200
