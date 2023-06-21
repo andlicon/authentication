d29448a176a4961f98836ba047f0b833536298df
andlicon <andresjzabalac@hotmail.com>
2023-06-21 10:17:48 -0400

    FIX: html structure

--- src/front/js/pages/Home.jsx
+++ src/front/js/pages/Home.jsx
@@ -8,8 +8,12 @@ export const Home = () => {
 
   return (
     <div className="container text-center pt-5">
-      <h1>Authentication system with Python Flask and React.js</h1>
-      <p>Almost every website in the world has user authentication, in this project you have to implement user authentication using the Python Flask framework for building a backend REST API and React.js and sessionStorage API for the front end web application.</p>
+      <h1>
+        Authentication system with Python Flask and React.js
+      </h1>
+      <p>
+        Almost every website in the world has user authentication, in this project you have to implement user authentication using the Python Flask framework for building a backend REST API and React.js and sessionStorage API for the front end web application.
+      </p>
     </div>
   );
 };
