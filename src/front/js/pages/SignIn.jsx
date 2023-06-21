7fde671ebefd9524df418cc7a1023a8d43f1ba5b
andlicon <andresjzabalac@hotmail.com>
2023-06-21 11:09:39 -0400

    ADD: if u are logged, can access to public routes

--- src/front/js/pages/SignIn.jsx
+++ src/front/js/pages/SignIn.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Context } from '../store/appContext.js'
 import '../../styles/signin.css';
 
 const SignIn = () => {
+  const navigate = useNavigate();
+  const { store } = useContext(Context);
+  const { token } = store
+
+  useEffect(() => {
+    if (token != null) {
+      navigate('/private')
+    }
+  }, []);
+
   return (
     <div className='container container--centrado'>
       INICIA SESIÓN
--- src/front/js/pages/SignUp.jsx
+++ src/front/js/pages/SignUp.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Context } from '../store/appContext.js'
 // component
 import SignUpForm from '../component/SignUpForm.jsx'
 
 const SignUp = () => {
+  const navigate = useNavigate();
+  const { store } = useContext(Context);
+  const { token } = store
+
+  useEffect(() => {
+    if (token != null) {
+      navigate('/private')
+    }
+  }, []);
+
   return (
     <div className='container container--centrado'>
       <SignUpForm />
