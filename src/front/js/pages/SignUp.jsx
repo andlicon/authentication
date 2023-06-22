5c9e2d99754c31855129e19de86fed122c5d0577
andlicon <andresjzabalac@hotmail.com>
2023-06-22 11:55:15 -0400

    FIX: sign up migrated to sing up page

--- src/front/js/pages/SignUp.jsx
+++ src/front/js/pages/SignUp.jsx
@@ -1,15 +1,50 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Context } from '../store/appContext.js'
-// component
-import SignUpForm from '../component/SignUpForm.jsx'
+import { Context } from '../store/appContext.js';
+import { isValidPassword, isValidEmail } from '../util.js';
+import '../../styles/signup.css';
+
+const initialValues = {
+  'email': '',
+  'password': ''
+}
 
 const SignUp = () => {
   const navigate = useNavigate();
-  const { store } = useContext(Context);
-  const { token } = store
+  const { store, actions } = useContext(Context);
+  const [credentials, setCredentials] = useState(initialValues)
+
+  const onChangeHandler = ({ target }) => {
+    const name = target.name;
+
+    setCredentials({
+      ...credentials,
+      [name]: target.value
+    })
+  }
+
+  const submitHandler = async event => {
+    const { signUp } = actions;
+    event.preventDefault();
+
+    const { passwordValid, passwordMessage } = isValidPassword(credentials.password);
+    const { emailValidate, emailMessage } = isValidEmail(credentials.email);
+
+    if (!emailValidate) {
+      console.log(emailMessage)
+      return null;
+    }
+    if (!passwordValid) {
+      console.log(passwordMessage)
+      return null;
+    }
+
+    await signUp(credentials);
+  }
 
   useEffect(() => {
+    const { token } = store
+
     if (token != null) {
       navigate('/private')
     }
@@ -17,7 +52,47 @@ const SignUp = () => {
 
   return (
     <div className='container container--centrado'>
-      <SignUpForm />
+      <form
+        className='signupForm bg-light needs-validation'
+        onSubmit={submitHandler}>
+
+        <h2 className='signupForm-title'>
+          Sign Up
+        </h2>
+        <div className='group'>
+          <label htmlFor='email' className='group-label'>
+            Email
+          </label>
+          <input
+            type='text'
+            id='email'
+            className='group-input'
+            name='email'
+            placeholder='doe@hotmail.com'
+            value={credentials.email}
+            onChange={onChangeHandler} />
+          <div id='emailFeedback' className='invalid-feedback'>
+            Please enter a message in the textarea.
+          </div>
+        </div>
+        <div className='group'>
+          <label htmlFor='password' className='group-label'>
+            Password
+          </label>
+          <input
+            type='password'
+            id='password'
+            className='group-input'
+            name='password'
+            value={credentials.password}
+            placeholder='********'
+            onChange={onChangeHandler} />
+        </div>
+
+        <button>
+          Sign Up
+        </button>
+      </form>
     </div>
   )
 }
