cae7c6b2e96ebc25fbafae43464072ddb0af953c
andlicon <andresjzabalac@hotmail.com>
2023-06-23 10:45:20 -0400

    ADD: if there isnt token then redirect to denied

--- src/front/js/pages/PostDetails.jsx
+++ src/front/js/pages/PostDetails.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const PostDetails = ({ post }) => {
+
+  useEffect(() => {
+    const { token } = store
+
+    if (token == null) {
+      navigate('/denied')
+    }
+  }, []);
+
   return (
     <>
       DETALLE
