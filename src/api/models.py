312aee50499de3dba3909e95030de83c295babab
andlicon <andresjzabalac@hotmail.com>
2023-06-23 08:32:50 -0400

    ADD: Post and Message models

--- src/api/models.py
+++ src/api/models.py
@@ -8,6 +8,9 @@ class User(db.Model):
     password = db.Column(db.String(250), nullable=False)
     salt = db.Column(db.String(80), nullable=False)
 
+    post = db.relationship('Post', backref='user', uselist=True)
+    message = db.relationship('Message', backref='user')
+
     def __repr__(self):
         return f'<User {self.email}>'
 
@@ -15,4 +18,40 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email
+        }
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    title = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(250), nullable=False)
+
+    message = db.relationship('Message', backref='post', uselist=True)
+
+    def __repr__(self):
+        return f'<Post {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "title": self.title,
+            "description": self.description
+        }
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    content = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return f'<Message {self.id}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "post_id": self.post_id,
+            "content": self.content
         }
