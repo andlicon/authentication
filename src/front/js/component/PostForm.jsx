083321cec00b10f4d532118b6c759aa0e7871b78
andlicon <andresjzabalac@hotmail.com>
2023-06-26 08:40:06 -0400

    ADD: postForm is a controlled component

--- src/front/js/component/PostForm.jsx
+++ src/front/js/component/PostForm.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../styles/postForm.css';
 
+const initialValues = {
+  'title': '',
+  'description': ''
+}
+
 const PostForm = () => {
+  const [postInfo, setPostInfo] = useState(initialValues);
+
+  const changeHandler = ({ target }) => {
+    setPostInfo({
+      ...postInfo,
+      [target.name]: target.value
+    })
+  }
+
   return (
     <form className='postForm'>
       <h2 className='postForm__title text-primary'>
@@ -11,15 +25,30 @@ const PostForm = () => {
         <label htmlFor='title' className='group-label'>
           Title
         </label>
-        <input type='text' id='title' className='group-input form-control' />
+        <input
+          type='text'
+          id='title'
+          name='title'
+          placeholder='Title example'
+          value={postInfo['title']}
+          onChange={changeHandler}
+          className='group-input form-control' />
       </div>
       <div className='form-group group'>
         <label htmlFor='description' className='group-label'>
           Description
         </label>
-        <textarea name='' id='description' className='group-input form-control'></textarea>
+        <textarea
+          name='description'
+          id='description'
+          placeholder='Description example'
+          value={postInfo['description']}
+          onChange={changeHandler}
+          className='group-input form-control'></textarea>
       </div>
-      <button type='button' className='btn btn-primary group-input form-control w-50 postform__submit'>
+      <button
+        type='button'
+        className='btn btn-primary group-input form-control w-50 postform__submit'>
         Post it!
       </button>
     </form>
