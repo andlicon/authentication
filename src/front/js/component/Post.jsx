bc8caf49367575e98a40ee5055d23eddd8f10810
andlicon <andresjzabalac@hotmail.com>
2023-06-26 10:06:07 -0400

    ADD: post styles

--- src/front/js/component/Post.jsx
+++ src/front/js/component/Post.jsx
@@ -5,22 +5,26 @@ import '../../styles/post.css';
 
 const Post = ({ post }) => {
   return (
-    <div className='post'>
-      <Link className='post__title' to={`/private/${post.id}`}>
-        {
-          post.title
-        }
-      </Link>
-      <p className='post__author'>
-        {
-          post.user_id
-        }
-      </p>
-      <p className='post__description'>
-        {
-          post.description
-        }
-      </p>
+    <div className='post bg-light'>
+      <div className='post__header'>
+        <Link className='post__title' to={`/private/${post.id}`}>
+          {
+            post.title
+          }
+        </Link>
+      </div>
+      <div className="post__content">
+        <p className='post__author'>
+          {
+            post.user_id
+          }
+        </p>
+        <p className='post__description'>
+          {
+            post.description
+          }
+        </p>
+      </div>
     </div>
   );
 };
